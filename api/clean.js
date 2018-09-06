08149ccd62158ea4981f720a12092498a0eb0042
Anastasia <anastasia.diudina@gmail.com>
2018-09-06 11:55:48 +0300

    Add functionto clean arithmetic expression

---
+++ api/clean.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const _ = require('lodash');
+
+const constants = require('../constants');
+
+const cleanSymbol = (symbol) => {
+  // Convert number to integer, validate that operation is supported
+  // and throw error if symbol is not supported
+  // Warning: do not support empty string
+  if (_.isInteger(+symbol)) { return parseInt(symbol); }
+  if (constants.SUPPORTED_ARITHMETIC_SYMBOLS.includes(symbol)) {
+    return symbol;
+  }
+  throw new Error(`Symbol "${symbol}" is not supported`);
+}
+
+module.exports = (expression) => {
+  // Clean arithmetic expression
+  // Throws error if non integer or not supported symbol found
+  return expression.map(cleanSymbol)
+};
---
+++ tests/clean.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const expect = require('chai').expect;
+
+const clean = require('../api/clean');
+
+describe('Clean arythmetic expression', () => {
+
+  it('Nmbers are cleaned correctly', () => {
+    const query = ['2'];
+    expect(clean(query)).to.be.deep.equal([2]);
+  });
+
+  it('Arithmetic operations are cleaned correctly', () => {
+    const query = ['2', '+', '10', '-', '11', '*', '12' / '2'];
+    expect(clean(query))
+      .to.be.deep.equal([2, '+', 10, '-', 11, '*', 12 / 2]);
+  });
+
+  it('Throw error if unknown symbol found', () => {
+    const query = ['2.12', '+', '12'];
+    expect(() => clean(query))
+      .to.throw('Symbol "2.12" is not supported');
+  });
+
+});

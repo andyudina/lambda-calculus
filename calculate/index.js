20672c5cbfe244a293374fa60e932ddb7edda995
Anastasia <anastasia.diudina@gmail.com>
2018-09-07 19:41:55 +0300

    Use decimals instead of floats

--- calculate/index.js
+++ calculate/index.js
@@ -4,6 +4,8 @@
 
 const _ = require('lodash')
 
+const Decimal = require('decimal.js')
+
 const infixToPostfix = require('./infixToPostfix')
 
 const constants = require('../constants')
@@ -11,13 +13,15 @@ const constants = require('../constants')
 const CalculateError = require('./error').CalculateError
 
 const SUPPORTED_OPERATIONS = {
-  [constants.MULTIPLICATION]: (first, second) => first * second,
+  [constants.MULTIPLICATION]: (first, second) => first.mul(second),
   [constants.DIVISION]: (first, second) => {
-    if (second === 0) { throw new CalculateError('Can not divide by zero') }
-    return first / second
+    if (second.equals(0)) {
+      throw new CalculateError('Can not divide by zero')
+    }
+    return first.div(second)
   },
-  [constants.ADDITION]: (first, second) => first + second,
-  [constants.SUBTRACTION]: (first, second) => first - second
+  [constants.ADDITION]: (first, second) => first.plus(second),
+  [constants.SUBTRACTION]: (first, second) => first.minus(second)
 }
 
 const calculateOperation = (firstOperand, secondOperand, operation) => {
@@ -33,12 +37,12 @@ const processOneSymbol = (symbol, calculationStack) => {
   // Returns updated calculation stack
   // Add number to stack
   if (_.isNumber(symbol)) {
-    return [...calculationStack, symbol]
+    return [...calculationStack, new Decimal(symbol)]
   }
   // Get last two numbers from stack and perform operation using them
   const secondOperand = calculationStack[calculationStack.length - 1]
   const firstOperand = calculationStack[calculationStack.length - 2]
-  if (!_.isNumber(secondOperand) || !_.isNumber(firstOperand)) {
+  if (!Decimal.isDecimal(secondOperand) || !Decimal.isDecimal(firstOperand)) {
     throw new CalculateError('Postfix expression has invalid format')
   }
   const result = calculateOperation(firstOperand, secondOperand, symbol)
